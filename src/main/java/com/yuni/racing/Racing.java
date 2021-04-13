ea42b96b16717f5016485915cc8c9da1b4dec35c
Eunice <lovepatty57@gmail.com>
2021-04-13 15:22:47 +0800

    new

---
+++ src/main/java/com/yuni/racing/Racing.java
@@ -0,0 +1,11 @@
+package com.yuni.racing;
+
+public class Racing {
+    public static  void main(String[] args){
+        Horse h1 = new Horse();
+        Horse h2 = new Horse();
+        h2.start();
+        h1.start();
+
+}
+}
