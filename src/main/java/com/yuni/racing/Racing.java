489185bd7259748b3209bf0649caee9190d81296
Eunice <lovepatty57@gmail.com>
2021-04-13 16:03:09 +0800

    new2

--- src/main/java/com/yuni/racing/Racing.java
+++ src/main/java/com/yuni/racing/Racing.java
@@ -4,8 +4,10 @@ public class Racing {
     public static  void main(String[] args){
         Horse h1 = new Horse();
         Horse h2 = new Horse();
+        Horse2 h3 = new Horse2();
+        Thread th = new Thread(h3);
+        th.start();
         h2.start();
         h1.start();
-
 }
 }
